e57e6a9b7e6a615cd0a9131b2527ec57797e99c5
Reginaldo de Jesus <reginaldosilva007@hotmail.com>
2025-01-20 17:10:15 -0300

    Seed Game

--- src/main/java/com/dev_superior/dslist/entities/Game.java
+++ src/main/java/com/dev_superior/dslist/entities/Game.java
@@ -17,9 +17,14 @@ public class Game {
     @Column(name="game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
@@ -29,7 +34,8 @@ public class Game {
                 String longDescription,
                 String shortDescription,
                 String imgUrl,
-                String platform,
+                String platforms,
+                Double score,
                 String genre,
                 Integer year,
                 String title) {
@@ -38,7 +44,8 @@ public class Game {
         this.longDescription = longDescription;
         this.shortDescription = shortDescription;
         this.imgUrl = imgUrl;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.genre = genre;
         this.year = year;
         this.title = title;
@@ -76,12 +83,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
